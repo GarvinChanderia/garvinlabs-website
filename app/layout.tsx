118577032c627df67a5b4fc84bbc5ed157845534
Garvin Chanderia <garvinclaw@gmail.com>
2026-05-03 12:48:38 +0530

    Fix: Refactored Google Translate implementation using next/script for reliability and better CSS handling.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Figtree, Outfit, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import SchemaOrg from "@/components/SchemaOrg";
+import Script from "next/script";
 
 const figtree = Figtree({
   subsets: ["latin"],
@@ -35,21 +36,20 @@ export default function RootLayout({
       <body className={`${figtree.variable} ${outfit.variable} ${jetbrainsMono.variable}`}>
         <SchemaOrg />
         {children}
-        <script
-          type="text/javascript"
+        <Script
           src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
           strategy="afterInteractive"
-        ></script>
-        <script
-          id="google-translate-init"
-          dangerouslySetInnerHTML={{
-            __html: `
-              function googleTranslateElementInit() {
-                new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
-              }
-            `,
-          }}
-        ></script>
+        />
+        <Script id="google-translate-init" strategy="afterInteractive">
+          {`
+            window.googleTranslateElementInit = function() {
+              new window.google.translate.TranslateElement({
+                pageLanguage: 'en',
+                layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE
+              }, 'google_translate_element');
+            }
+          `}
+        </Script>
       </body>
     </html>
   );
